128c49310d8bee0be9b200f4e4ce7ca9dc5a3299
Travis Smith <Travis.Smith2@gmail.com>
2018-05-20 15:02:34 -0400

    added html routing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-var apiRoutes = require("./app/routing/api-routes.js");
-var htmlRoutes = require("./app/routing/html-routes.js");
+//var apiRoutes = require("./routing/apiRoutes.js");
+var htmlRoutes = require("./routing/htmlRoutes.js");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-apiRoutes(app);
+//apiRoutes(app);
 htmlRoutes(app);
 
 app.listen(PORT, function() {
